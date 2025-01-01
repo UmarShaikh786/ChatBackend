a54fddcd9723f32db9e443445de138d2224a61ba
Umar Shaikh <110691294+UmarShaikh786@users.noreply.github.com>
2025-01-01 15:11:51 +0530

    Frontend Url set in Environment variable

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,11 @@ const socket = require('socket.io');
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: process.env.Frontend_URL, // Change this to the frontend URL
+    credentials: true,
+
+}));
 app.use(express.json());
 
 // Default Route
